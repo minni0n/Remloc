27c99d42fd026bf644e2139bac9ec22d32622b12
minni0n <minnion1337@gmail.com>
2022-05-18 20:35:05 +0200

    some bigger app

--- app/src/main/java/com/example/remloc1/HomeActivity.kt
+++ app/src/main/java/com/example/remloc1/HomeActivity.kt
@@ -1,22 +1,30 @@
 package com.example.remloc1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class HomeActivity : AppCompatActivity() {
 
+    lateinit var mGoogleSignInClient: GoogleSignInClient
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
@@ -27,21 +35,51 @@ class HomeActivity : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
 
+            it.isChecked = true
+
             when(it.itemId){
 
-                R.id.nav_places -> Toast.makeText(applicationContext, "Clicked Miejsca", Toast.LENGTH_SHORT).show()
-                R.id.nav_actions -> Toast.makeText(applicationContext, "Clicked Akcje", Toast.LENGTH_SHORT).show()
-                R.id.nav_game_miejska -> Toast.makeText(applicationContext, "Clicked Gra miejska", Toast.LENGTH_SHORT).show()
-                R.id.nav_settings -> Toast.makeText(applicationContext, "Clicked Settings", Toast.LENGTH_SHORT).show()
-                R.id.nav_help_review -> Toast.makeText(applicationContext, "Clicked Pomoc i opinie", Toast.LENGTH_SHORT).show()
-                R.id.nav_logout -> Toast.makeText(applicationContext, "Clicked Logout", Toast.LENGTH_SHORT).show()
-
+                R.id.nav_places -> replaceFragment(PlacesFragment(), it.title.toString())
+                R.id.nav_actions -> replaceFragment(ActionsFragment(), it.title.toString())
+                R.id.nav_game_miejska -> replaceFragment(GameMiejskaFragment(), it.title.toString())
+                R.id.nav_settings -> replaceFragment(SettingsFragment(), it.title.toString())
+                R.id.nav_help_review -> replaceFragment(HelpReviewFragment(), it.title.toString())
+                R.id.nav_logout -> logoutFromGoogle()
             }
 
             true
 
 
         }
+
+        // call requestIdToken as follows
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+    }
+
+    private fun logoutFromGoogle() {
+        mGoogleSignInClient.signOut().addOnCompleteListener {
+            val intent = Intent(this, MainActivity::class.java)
+            Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment, title: String){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
 
     }
 
